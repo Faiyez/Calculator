c1145a94305518b0d6d8d5f891fc5806d07f499c
Faiyez <90771442+Faiyez@users.noreply.github.com>
2022-01-01 13:54:52 -0330

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class Calculator implements ActionListener{
+    JFrame frame;
+    JTextField textField;
+    JButton[] numberButton = new JButton[10];
+    JButton[] functionButton = new JButton[8];
+    JButton addButton, subButton, mulButton, divButton;
+    JButton decButton, equButton, delButton, clrButton;
+    JPanel panel;
+    Font myFont = new Font("Ink Free", Font.BOLD,30);
+    double num1 = 0, num2 = 0, result = 0;
+    char operator; 
+    Calculator(){
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
+        frame.setLayout(null);
+        textField = new JTextField();
+        textField.setBounds(50,25,300,50);
+        textField.setFont(myFont);
+        frame.add(textField);
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args){
+        Calculator calc = new Calculator();
+        
+    }
+    
+
+
+    public void actionPerformed(ActionEvent e){
+
+    }
+} 
